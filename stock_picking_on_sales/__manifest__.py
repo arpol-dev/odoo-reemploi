e2021b14eae38612679146001ea3e346bdc7cfd7
ArPol <armand@arpol.fr>
2024-09-17 10:23:09 +0200

    Create stock_picking_on_sales, adding a selection option on stock.picking model

---
+++ stock_picking_on_sales/__manifest__.py
@@ -0,0 +1,15 @@
+# @author: Armand Polmard (<Link>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Création de relevés de banque automatique",
+    "summary": "Créé et enrichi des relevés de banque à la création des transactions bancaires dans un journal.",
+    "version": "16.0.0.0.0",
+    "category": "Accounting/Accounting", 
+    "author": "ArPol",
+    "website": "https://github.com/arpol-dev",
+    "license": "AGPL-3",
+    "depends": [
+        "account_reconcile_oca",
+    ],
+}
---
+++ stock_picking_on_sales/models/stock_picking.py
@@ -0,0 +1,17 @@
+# Copyright 2024 ArPol
+# @author: Armand Polmard <armand@arpol.fr>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+import datetime
+import logging
+
+
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    code = fields.Selection(
+        selection_add=[
+            ("from_sale", "Collecte"),
+        ]
+    )
