9c4b81a65d1bc3b268a7ffdb0d82efdea877911c
ArPol <armand@arpol.fr>
2024-09-17 14:48:45 +0200

    Corrections on stock_picking_on_sales

--- stock_picking_on_sales/__manifest__.py
+++ stock_picking_on_sales/__manifest__.py
@@ -1,15 +1,16 @@
-# @author: Armand Polmard (<Link>)
+# @author: Armand Polmard (armand@arpol.fr)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Création de relevés de banque automatique",
-    "summary": "Créé et enrichi des relevés de banque à la création des transactions bancaires dans un journal.",
+    "name": "Entrées d'inventaire sur ventes",
+    "summary": "Permet de gérer des mouvements d'entrée dans l'inventaire liés à des ventes de services",
     "version": "16.0.0.0.0",
-    "category": "Accounting/Accounting", 
+    "category": "Inventory/Inventory", 
     "author": "ArPol",
     "website": "https://github.com/arpol-dev",
     "license": "AGPL-3",
     "depends": [
-        "account_reconcile_oca",
+        "stock",
+        "sale_management",
     ],
 }
